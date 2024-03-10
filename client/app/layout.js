e84832f98ca9f511e7d56a46d322fb7cefe21862
Imtiaz_1 <smimtiaz58@gmail.com>
2024-03-11 04:16:39 +0600

    Day 1 - Dashboard page half done

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,7 +1,10 @@
-import { Inter } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
+import ReactQueryProvider from "@/utils/Provider";
+import Navbar from "@/component/Navbar";
+import DiscountBar from "@/component/DiscountBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const monserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={monserrat.className}>
+        <ReactQueryProvider>
+          <DiscountBar />
+          <Navbar />
+          <div className="global_padding">{children}</div>
+        </ReactQueryProvider>
+      </body>
     </html>
   );
 }
--- client/app/page.js
+++ client/app/page.js
@@ -1,7 +1,19 @@
-import React from "react";
+import { Box } from "@mui/material";
+import Image from "next/image";
+
+import Slider from "@/component/Slider";
+import discountImage from "../public/discountBanner.png";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Slider />
+
+      <Box>
+        <Image alt="banner" src={discountImage} />
+      </Box>
+    </>
+  );
 };
 
 export default App;
